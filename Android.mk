2f50810b7b7aeedb08fd2cfda716eb7131f597d1
martincz <martincz@mokeedev.com>
2016-10-08 23:32:08 +0800

    MKParts: This is MK!
    Change-Id: I7b0213a401d641651297201878eec2627524ba6b

--- Android.mk
+++ Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_PACKAGE_NAME := CMParts
+LOCAL_PACKAGE_NAME := MKParts
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_STATIC_JAVA_LIBRARIES := \
     android-support-v4 \
@@ -12,7 +12,7 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
     android-support-v7-appcompat \
     android-support-v14-preference \
     jsr305 \
-    org.cyanogenmod.platform.internal
+    org.mokee.platform.internal
 
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/res \
