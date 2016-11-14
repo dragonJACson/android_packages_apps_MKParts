e382141612c7469cbde3036b819df0eef7c695a4
martincz <martincz@mokeedev.com>
2016-11-14 17:39:06 +0800

    MKParts: Add MoKee Push Service
    Change-Id: I26b11097d794a667b151ce75df2d6c74e3cb8e7e

--- Android.mk
+++ Android.mk
@@ -12,6 +12,7 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
     android-support-v7-appcompat \
     android-support-v14-preference \
     jsr305 \
+    mokee-push \
     uicommon \
     org.mokee.platform.internal
 
---
+++ src/org/mokee/mkparts/push/PushingMessageReceiver.java
@@ -0,0 +1,143 @@
+/*
+ * Copyright (C) 2014-2016 The MoKee Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.mokee.mkparts.push;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.mokee.mkparts.R;
+
+import android.app.Notification;
+import android.app.Notification.BigTextStyle;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.mokee.utils.MoKeeUtils;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.util.Log;
+import cn.jpush.android.api.JPushInterface;
+
+import com.mokee.os.Build;
+
+public class PushingMessageReceiver extends BroadcastReceiver {
+
+    public static final String TAG = PushingMessageReceiver.class.getSimpleName();
+
+    public static final String MKPUSH_PREF = "mokee_push";
+    public static final String MKPUSH_ALIAS = "pref_alias";
+    public static final String MKPUSH_TAGS = "pref_tags";
+    public static final String MKPUSH_NEWS = "pref_news";
+
+    public static final int MSG_SET_ALIAS = 1001;
+    public static final int MSG_SET_TAGS = 1002;
+
+    private static final String ACTION_UPDATE_CHECK = "com.mokee.mkupdater.action.UPDATE_CHECK";
+
+    private SharedPreferences prefs;
+
+    @Override
+    public void onReceive(Context ctx, Intent intent) {
+        Bundle bundle = intent.getExtras();
+        Log.d(TAG, "[MyReceiver] onReceive - " + intent.getAction() + ", extras: " + printBundle(bundle));
+        if (JPushInterface.ACTION_MESSAGE_RECEIVED.equals(intent.getAction())) {
+            String message = bundle.getString(JPushInterface.EXTRA_MESSAGE);
+            String customContentString = bundle.getString(JPushInterface.EXTRA_EXTRA);
+            onMessage(ctx, message, customContentString);
+            JPushInterface.reportNotificationOpened(ctx, bundle.getString(JPushInterface.EXTRA_MSG_ID));
+        }
+    }
+
+    public void onMessage(Context ctx, String message, String customContentString) {
+        if (customContentString != null & customContentString != "") {
+            JSONObject customJson = null;
+            try {
+                customJson = new JSONObject(customContentString);
+            } catch (JSONException e1) {
+                e1.printStackTrace();
+            }
+            prefs = ctx.getSharedPreferences(MKPUSH_PREF, 0);
+            String device = PushingUtils.getStringFromJson("device", customJson);
+            String type = PushingUtils.getStringFromJson("type", customJson);
+            String url = PushingUtils.getStringFromJson("url", customJson);
+            String title = PushingUtils.getStringFromJson("title", customJson);
+            String newVersion = PushingUtils.getStringFromJson("version", customJson);
+            int msg_id = PushingUtils.getIntFromJson("id", customJson);
+            String currentDevice = Build.PRODUCT.toLowerCase();
+            String currentVersion = Build.VERSION.toLowerCase();
+
+            if (PushingUtils.allowPush(device, currentDevice, 1) && PushingUtils.allowPush(type, currentVersion, 0)
+                    || device.equals("all") && type.equals("all")
+                    || device.equals("all") && PushingUtils.allowPush(type, currentVersion, 0)
+                    || PushingUtils.allowPush(device, currentDevice, 1) && type.equals("all")) {
+                switch (msg_id) {
+                    case 0:
+                        String mod_version_code = currentVersion.split("-")[2];
+                        int new_version_code = Integer.parseInt(newVersion);
+                        if (mod_version_code.length() > 6) {
+                            mod_version_code = mod_version_code.substring(2, 8);
+                        }
+                        if (new_version_code > Integer.parseInt(mod_version_code)) {
+                            Intent intent = new Intent(ACTION_UPDATE_CHECK);
+                            ctx.sendBroadcastAsUser(intent, UserHandle.CURRENT);
+                        }
+                        break;
+                    case 1:
+                        if (MoKeeUtils.isSupportLanguage(true) && prefs.getBoolean(MKPUSH_NEWS, true)) {
+                            promptUser(ctx, url, title, message, msg_id, R.drawable.ic_push_notify);
+                        }
+                        break;
+                }
+            }
+        }
+    }
+
+    private void promptUser(Context context, String url, String title, String message, int id, int icon) {
+        NotificationManager nm = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        Uri uri = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        PendingIntent pendintIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+        BigTextStyle noti = new Notification.BigTextStyle(new Notification.Builder(context)
+                .setSmallIcon(icon).setAutoCancel(true).setTicker(title)
+                .setContentIntent(pendintIntent).setWhen(0).setContentTitle(title)
+                .setColor(context.getColor(com.android.internal.R.color.system_notification_accent_color))
+                .setDefaults(Notification.DEFAULT_SOUND | Notification.DEFAULT_LIGHTS)
+                .setContentText(message)).bigText(message);
+
+        nm.notify(id, noti.build());
+    }
+
+    // 打印所有的 intent extra 数据
+    private static String printBundle(Bundle bundle) {
+        StringBuilder sb = new StringBuilder();
+        for (String key : bundle.keySet()) {
+            if (key.equals(JPushInterface.EXTRA_NOTIFICATION_ID)) {
+                sb.append("\nkey:" + key + ", value:" + bundle.getInt(key));
+            } else if (key.equals(JPushInterface.EXTRA_CONNECTION_CHANGE)) {
+                sb.append("\nkey:" + key + ", value:" + bundle.getBoolean(key));
+            } 
+            else {
+                sb.append("\nkey:" + key + ", value:" + bundle.getString(key));
+            }
+        }
+        return sb.toString();
+    }
+}
