7c7017ea32ed1f09880d591173933a49e41bbb89
Martincz Gao <martincz@mokeedev.com>
2019-01-06 04:07:36 +0800

    MKParts: Refactor the NetworkTraffic (2/2)
    Change-Id: I7c45c8c42a2ed78118b6aee507efacaaf8fe170c

--- src/org/mokee/mkparts/statusbar/NetworkTrafficSettings.java
+++ src/org/mokee/mkparts/statusbar/NetworkTrafficSettings.java
@@ -1,5 +1,6 @@
 /*
- * Copyright (C) 2017-2018 The LineageOS Project
+ * Copyright (C) 2014-2019 The MoKee Project
+ * Copyright (C) 2017-2019 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -34,8 +35,6 @@ public class NetworkTrafficSettings extends SettingsPreferenceFragment
 
     private DropDownPreference mNetTrafficMode;
     private MKSecureSettingSwitchPreference mNetTrafficAutohide;
-    private DropDownPreference mNetTrafficUnits;
-    private MKSecureSettingSwitchPreference mNetTrafficShowUnits;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -54,17 +53,6 @@ public class NetworkTrafficSettings extends SettingsPreferenceFragment
                 findPreference(MKSettings.Secure.NETWORK_TRAFFIC_AUTOHIDE);
         mNetTrafficAutohide.setOnPreferenceChangeListener(this);
 
-        mNetTrafficUnits = (DropDownPreference)
-                findPreference(MKSettings.Secure.NETWORK_TRAFFIC_UNITS);
-        mNetTrafficUnits.setOnPreferenceChangeListener(this);
-        int units = MKSettings.Secure.getInt(resolver,
-                MKSettings.Secure.NETWORK_TRAFFIC_UNITS, /* MB */ 3);
-        mNetTrafficUnits.setValue(String.valueOf(units));
-
-        mNetTrafficShowUnits = (MKSecureSettingSwitchPreference)
-                findPreference(MKSettings.Secure.NETWORK_TRAFFIC_SHOW_UNITS);
-        mNetTrafficShowUnits.setOnPreferenceChangeListener(this);
-
         updateEnabledStates(mode);
     }
 
@@ -75,10 +63,6 @@ public class NetworkTrafficSettings extends SettingsPreferenceFragment
             MKSettings.Secure.putInt(getActivity().getContentResolver(),
                     MKSettings.Secure.NETWORK_TRAFFIC_MODE, mode);
             updateEnabledStates(mode);
-        } else if (preference == mNetTrafficUnits) {
-            int units = Integer.valueOf((String) newValue);
-            MKSettings.Secure.putInt(getActivity().getContentResolver(),
-                    MKSettings.Secure.NETWORK_TRAFFIC_UNITS, units);
         }
         return true;
     }
@@ -86,7 +70,5 @@ public class NetworkTrafficSettings extends SettingsPreferenceFragment
     private void updateEnabledStates(int mode) {
         final boolean enabled = mode != 0;
         mNetTrafficAutohide.setEnabled(enabled);
-        mNetTrafficUnits.setEnabled(enabled);
-        mNetTrafficShowUnits.setEnabled(enabled);
     }
 }
